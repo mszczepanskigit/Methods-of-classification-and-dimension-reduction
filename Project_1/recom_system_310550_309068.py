d833a8d313a17df04c02cc699fab2279d7eba1b5
mszczepanskigit <mateusz.szczepanski.kontakt@gmail.com>
2023-03-22 21:48:24 +0100

    Test and training sets

--- Project_1/recom_system_310550_309068.py
+++ Project_1/recom_system_310550_309068.py
@@ -3,7 +3,8 @@ import numpy as np
 from sklearn.decomposition import NMF
 from sklearn.decomposition import TruncatedSVD
 import sys
-
+import random as rd
+import math
 
 """
 Questions:
@@ -54,6 +55,7 @@ def SGD():
 
 
 if __name__ == "__main__":
+    rd.seed(0)
     args = parsing()
     trains_file = args.train
     tests_file = args.test
@@ -61,18 +63,30 @@ if __name__ == "__main__":
     result_file = args.result
 
     with open('./ratings.csv', 'r') as ratings, \
-            open(f'{tests_file}', 'r') as test_file, \
-            open(f'{trains_file}', 'r') as train_file:
+            open(f'{tests_file}', 'w') as test_file, \
+            open(f'{trains_file}', 'w') as train_file:
         ratings = np.genfromtxt(ratings, delimiter=',')[1:]
-        test_file = np.genfromtxt(test_file, delimiter=',')[1:]
-        train_file = np.genfromtxt(train_file, delimiter=',')[1:]
 
         matrix = np.zeros((int(np.max(ratings[:, 0])), int(np.max(ratings[:, 1]))))
         # print(matrix.shape) # = (610, 193609)
         for row in ratings:
             matrix[int(row[0]) - 1, int(row[1]) - 1] = row[2]
-        matrix = matrix.astype('uint8')
-        print(matrix) # it's filled
+        # matrix = matrix.astype('uint8')
+        print(matrix)  # it's filled
+        """test_file.write("userId,movieId,rating,timestamp\n")
+        train_file.write("userId,movieId,rating,timestamp\n")
+        i = 1
+        for user in matrix:
+            movie_temp = [movieID for movieID in range(len(user)) if matrix[i - 1, movieID] > 0]
+            train = rd.sample(movie_temp, 1 + math.floor(0.9 * len(movie_temp)))
+            train.sort()
+            test = list(set(movie_temp).difference(set(train)))
+            test.sort()
+            for train_movie in train:
+                train_file.write(f"{i},{train_movie},{matrix[i - 1, train_movie]},1\n")
+            for test_movie in test:
+                test_file.write(f"{i},{test_movie},{matrix[i - 1, test_movie]},1\n")
+            i += 1"""
 
         # Proceeding
         if alg == "NMF":
@@ -80,9 +94,9 @@ if __name__ == "__main__":
         elif alg == "SVD1":
             result = len(alg)
         elif alg == "SVD2":
-            result = len(alg)+1
+            result = len(alg) + 1
         elif alg == "SGD":
-            result = len(alg)+2
+            result = len(alg) + 2
         else:
             sys.exit("Something went wrong.")
 
